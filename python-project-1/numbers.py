9a23133d168cf20aec3056a455fd4f2044d19883
abrifuh6 <abrifuh6@gmail.com>
2023-11-24 02:01:44 -0800

    numbers.py

---
+++ python-project-1/numbers.py
@@ -0,0 +1,6 @@
+# complex number example
+complex_number = 3 + 5j
+
+print("Complex Number:", complex_number)
+print("Real number Part:", complex_number.real)
+print("Imaginary Number Part:", complex_number.imag)
