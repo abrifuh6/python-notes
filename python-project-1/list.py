1a54d3a1de8787d9ab08cc3323d5842b41dc81dd
abrifuh6 <abrifuh6@gmail.com>
2023-11-24 03:50:23 -0800

    lists.py

---
+++ python-project-1/list.py
@@ -0,0 +1,9 @@
+# list of fruits
+fruits = ["apple", "banana", "pawpaw", "watermelon"]
+print("List Of Fruits:", fruits)
+
+first_fruit = fruits[0]
+second_fruit = fruits[3]
+
+print("What's the first fruit on the list? ", first_fruit)
+print("What's your favorite fruit on the list? ", second_fruit)
