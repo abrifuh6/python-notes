02c4a0e536e89c723aac0b8751e87e649de976f3
abrifuh6 <abrifuh6@gmail.com>
2023-11-25 12:25:59 -0800

    tuple.py

---
+++ python-project-1/tuple.py
@@ -0,0 +1,10 @@
+# tuples are surrounded by parenthesis `()` as seen below
+
+car = ("honda", "toyota", "kia", "audi")
+print(car[0])
+
+# because this is a tuple, it cannot be modified or deleted in the stack trace. 
+
+del (car)
+
+print(car)
