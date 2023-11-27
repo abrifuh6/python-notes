424c3af2b6d01a414f08151374f3651ff68f4f8b
abrifuh6 <abrifuh6@gmail.com>
2023-11-26 23:43:39 -0800

    .\dictionary.py

---
+++ python-project-1/dictionary.py
@@ -0,0 +1,11 @@
+# Dictionary is unordered list that stores data using "key-value pairs"
+# Here's an example of such
+student_info = {
+    "first_name" : "John" ,
+    "last_name" :" Doe ",
+    "age": 20 ,
+    "grade" : "A"
+}
+
+# student_name = (student_info["first_name"] )
+print (student_info["first_name"] )
