40ba157031dae19d24893ea370e56687655a5fe4
abrifuh6 <abrifuh6@gmail.com>
2024-07-16 23:55:50 -0700

    main.py

--- angela-python/main.py
+++ angela-python/main.py
@@ -1,3 +1,6 @@
 # 'len' will count the number of characters in a "string"
-numOfLetters = len("abri")
-print(numOfLetters)
+#numOfLetters = len("abri")
+#print(numOfLetters)
+
+name = input("What is your name? ")
+print(name)
